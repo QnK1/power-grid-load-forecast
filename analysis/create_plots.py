3aeddf1d476016227db1020de05a3304ead1e1fe
MrWlobo <michaldworniczak40@gmail.com>
2025-10-23 10:09:57 +0200

    Added PlotCreator class and create_line_charts method

---
+++ analysis/create_plots.py
@@ -0,0 +1,31 @@
+from utils.load_data import load_data, DataLoadingParams
+from matplotlib import pyplot as plt
+
+class PlotCreator:
+    def __init__(self, loading_params=DataLoadingParams()):
+        self.data = loading_params
+        self.df, self.raw_df = load_data(self.data)
+        self.path = "plots/"
+
+    def create_line_charts(self, x: list[str], y: list[str], filename: str):
+        if len(x) != len(y):
+            raise ValueError(f"Amount of parameters in x ({len(x)}) should be equal to the amount of parameters in y ({len(y)}).")
+
+        figure, axis = plt.subplots(1, len(x))
+
+        for i in range(len(x)):
+            axis[i].plot(self.raw_df[x[i]], self.raw_df[y[i]])
+            axis[i].set_xlabel(x[i])
+            axis[i].set_ylabel(y[i])
+            axis[i].tick_params(axis='x', rotation=90)
+        figure.tight_layout()
+        figure.savefig(f"{self.path}{filename}")
+        plt.show()
+
+# ----- TESTING -----
+data_params = DataLoadingParams()
+data_params.years = [2015]
+data_params.freq = "15min"
+
+plot_creator = PlotCreator(data_params)
+plot_creator.create_line_charts(["date", "date"], ["load", "temperature"], "load_and_temperature_plots")
