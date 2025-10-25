fixed data scaling to follow proper practise

utils/load_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pathlib import Path
 from sklearn.preprocessing import StandardScaler
 import numpy as np
-import tensorflow as tf
+from tensorflow import Tensor
 
 class DataLoadingParams:
     """Customizable parameters for the load_data function.
@@ -78,7 +78,7 @@ def load_test_data(params: DataLoadingParams) -> tuple[pd.DataFrame, pd.DataFram
     return _load_data(params, TEST_YEARS)
 
 
-def decode_ml_outputs(to_decode: tf.Tensor | pd.DataFrame, raw: pd.DataFrame):
+def decode_ml_outputs(to_decode: Tensor | pd.DataFrame, raw: pd.DataFrame):
     """
     Returns the power grid loads in MW corresponding to give scaled values.
     !!! IMPORTANT: raw has to be the same DataFrame returned by load_training_data
@@ -206,7 +206,7 @@ def _load_data(params, years):
     real_data_df = df.copy()
 
     # get ml-ready df
-    df = _get_ml_ready_df(df, params)
+    df = _get_ml_ready_df(df, params, years == TRAINING_YEARS)
 
     return df, real_data_df
 
@@ -425,7 +425,7 @@ def _handle_sliding_window_nans(df, params):
     return df
 
 
-def _get_ml_ready_df(df, params):
+def _get_ml_ready_df(df, params, is_training_data):
     # apply sine-cosine transformation for cyclical features
     df['hour_of_day_sin'] = np.sin(2 * np.pi * df['hour_of_day'] / df['hour_of_day'].max())
     df['hour_of_day_cos'] = np.cos(2 * np.pi * df['hour_of_day'] / df['hour_of_day'].max())
@@ -437,17 +437,27 @@ def _get_ml_ready_df(df, params):
     df['day_of_year_cos'] = np.cos(2 * np.pi * df['day_of_year'] / df['day_of_year'].max())
 
     df.drop(columns=['day_of_year', 'hour_of_day', 'day_of_week'], inplace=True)
-    df.drop(columns=['date'], inplace=True)
+    df.set_index('date', inplace=True)
 
     # standardize other features
     scaler = StandardScaler()
+    
+    # this is done to ensure the data is always scaled according to the training data
+    # it does not introduce data leakage, it emulates a model's pipeline
+    if not is_training_data:
+        _, training_df = load_training_data(params)
+    else:
+        training_df = df
+    
     cols_to_scale = [col for col in df.columns if col not in 
                         ['hour_of_day_sin', 'hour_of_day_cos', 'day_of_week_sin', 'day_of_week_cos',
-                            'day_of_year_sin', 'day_of_year_cos']]
+                            'day_of_year_sin', 'day_of_year_cos', 'date']]
 
-    scaled_values = scaler.fit_transform(df[cols_to_scale])
+    scaler.fit(training_df[cols_to_scale])
+    scaled_values = scaler.transform(df[cols_to_scale])
+    training_df = None
 
-    df_scaled = pd.DataFrame(scaled_values, columns=cols_to_scale)
+    df_scaled = pd.DataFrame(scaled_values, columns=cols_to_scale, index=df.index)
 
     df_final = df.drop(cols_to_scale, axis=1)
     df_final = pd.concat([df_final, df_scaled], axis=1)