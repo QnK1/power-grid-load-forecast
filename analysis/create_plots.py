c6f4483d3215d0fb2806da79f2662e44d214ceeb
MrWlobo <michaldworniczak40@gmail.com>
2025-10-25 20:29:45 +0200

    test

--- analysis/create_plots.py
+++ analysis/create_plots.py
@@ -1,6 +1,7 @@
 from utils.load_data import load_raw_data
 from matplotlib import pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 class PlotCreator:
     """
@@ -17,6 +18,9 @@ class PlotCreator:
 
     Attributes:
         raw_df (pd.DataFrame): The raw data loaded for the specified years and months.
+        years (list[int]): List of years included in the dataset and used for plotting.
+        months (list[int]): List of months included in the dataset and used for filtering plots.
+        month_names (dict[int, str]): Mapping of month indices (0–11) to their English names.
         path (str): Directory path where plots will be saved. Default is "plots/"""
 
     def __init__(self, years: list[int], months: list[int]):
@@ -28,6 +32,24 @@ class PlotCreator:
         """
 
         self.raw_df = load_raw_data(years, months)
+        self.months = months
+        self.years = years
+
+        self.month_names = {
+            0: "January",
+            1: "February",
+            2: "March",
+            3: "April",
+            4: "May",
+            5: "June",
+            6: "July",
+            7: "August",
+            8: "September",
+            9: "October",
+            10: "November",
+            11: "December"
+        }
+
         self.path = "plots/"
 
     def create_line_chart_with_index(self, y: list[str], normalize: bool = False, save_plot: bool = False, filename: str = "new_plot") -> None:
@@ -84,6 +106,46 @@ class PlotCreator:
             figure.savefig(f"{self.path}{filename}")
         plt.show()
 
+    def create_correlation_matrix(self, save_plot: bool = False, filename: str = "new_correlation_matrix") -> None:
+        """
+        Generates and displays a correlation heatmap for all numeric columns in the raw DataFrame.
+
+        This method computes the pairwise correlation between all numerical features in the dataset
+        and visualizes the results as a color-coded heatmap using Seaborn. It can optionally save
+        the plot as an image file in the directory specified by the `path` attribute.
+
+        :param save_plot: If True, saves the plot as a PNG file to the configured directory. Default is False.
+        :param filename: The filename (without extension) to use when saving the plot. Default is "new_correlation_matrix".
+        :returns: None
+        """
+
+        sns.heatmap(self.raw_df.corr(), annot=True, cmap="coolwarm")
+        if save_plot:
+            plt.savefig(f"{self.path}{filename}")
+        plt.show()
+
+    def create_correlation_matrices_for_months(self, save_plot: bool = False, filename: str = "new_correlation_matrices") -> None:
+        figure, axis = plt.subplots(len(self.months), len(self.years))
+
+        for i, year in enumerate(self.years):
+            for j, month in enumerate(self.months):
+                sns.heatmap(self.raw_df[(self.raw_df.index.year == year) & (self.raw_df.index.month == month + 1)].corr(), annot=True, cmap="coolwarm", ax=axis[j, i], cbar=(i == len(self.years) - 1))
+
+                axis[j, i].set_title(f"{self.month_names[month]} {year}", fontsize=10)
+
+                if j < len(self.months) - 1:
+                    axis[j, i].set_xticklabels([])
+                    axis[j, i].set_xlabel("")
+                if i > 0:
+                    axis[j, i].set_yticklabels([])
+                    axis[j, i].set_ylabel("")
+
+        plt.tight_layout()
+        if save_plot:
+            plt.savefig(f"{self.path}{filename}")
+        plt.show()
+
+
     def _normalize_data(self, column: pd.Series) -> pd.Series:
         """
         Normalizes a Pandas Series to the 0–1 range using min-max scaling.
@@ -96,5 +158,7 @@ class PlotCreator:
 
 # ----- TESTING -----
 
-plot_creator = PlotCreator([2015], [2])
-plot_creator.create_line_chart_with_index(["load", "temperature"], True)
+plot_creator = PlotCreator([2015, 2016, 2017, 2018], [4, 5, 6, 7, 8])
+#plot_creator.create_line_chart_with_index(["load", "temperature"], True)
+#plot_creator.create_correlation_matrix()
+plot_creator.create_correlation_matrices_for_months()
