6d1795c0d2e845eaa015f3a8047bdfd3125b4f97
Rinchyen <bumbaa2001@gmail.com>
2025-10-26 19:40:37 +0100

    chore: saving before pull

--- models/committee_system.py
+++ models/committee_system.py
@@ -1,4 +1,4 @@
-from utils.load_data import load_training_data, DataLoadingParams
+# from utils.load_data import load_training_data, DataLoadingParams
 
 # models/committee_system.py
 
@@ -8,7 +8,7 @@ class CommitteeSystem:
     """
     Manages a committee of neural network models for ensemble forecasting.
     
-    This class holds multiple 'expert' models. It trains them and then averages 
+    This class holds multiple 'expert' models, trains them, and averages 
     their predictions to produce a final, combined forecast.
     """
     
@@ -17,8 +17,8 @@ class CommitteeSystem:
         Initializes the CommitteeSystem.
         
         Args:
-            models (list): A list of instantiated model objects 
-                           (e.g., [MLP(), LSTM(), ...]).
+            models (list): A list of instantiated model objects that have
+                           .train() and .predict() methods.
         """
         if not models:
             raise ValueError("The 'models' list cannot be empty.")
@@ -29,7 +29,7 @@ class CommitteeSystem:
 
     def train(self, X_train, y_train):
         """
-        Trains each model in the committee.
+        Trains each model in the committee on the provided data.
         
         Args:
             X_train: The training input data (features).
@@ -38,31 +38,23 @@ class CommitteeSystem:
         print(f"--- Training the {self.name} ---")
         for i, model in enumerate(self.models):
             print(f"Training expert model {i+1}/{len(self.models)}: {model.name}...")
-            # Each model is expected to have its own 'train' method.
-            # We will need to define this later for each expert model.
-            # For now, we'll assume it exists.
             model.train(X_train, y_train)
         print("--- All expert models have been trained. ---")
 
     def predict(self, X):
         """
-        Makes a prediction by averaging the predictions of all models.
+        Generates a prediction by averaging the outputs of all models.
         
         Args:
             X: The input data for which to make predictions.
             
         Returns:
-            An array of final, averaged predictions.
+            np.ndarray: An array containing the final, averaged predictions.
         """
         # Collect predictions from each model in the committee
-        predictions = []
-        for model in self.models:
-            # Each model is expected to have its own 'predict' method.
-            pred = model.predict(X)
-            predictions.append(pred)
+        all_predictions = [model.predict(X) for model in self.models]
         
-        # Average the predictions
-        # np.mean with axis=0 calculates the mean across the different models for each sample
-        final_predictions = np.mean(predictions, axis=0)
+        # Average the predictions across the models for each sample
+        final_predictions = np.mean(all_predictions, axis=0)
         
         return final_predictions
