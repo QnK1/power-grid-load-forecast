91f6b4f29ecfdab2f09ee6501eba1718ad42c3f4
MrWlobo <michaldworniczak40@gmail.com>
2025-11-02 09:47:36 +0100

    Moved the process of training a specific model to a temporary file trainer.py

---
+++ models/lstm/trainer.py
@@ -0,0 +1,26 @@
+from models.lstm.lstm import get_model, get_model_name, train_model, FEATURE_COLUMNS
+
+# Temporary file for creating LSTM models
+
+# lstm_layers = [8]
+# dense_layers = [8]
+# sequence_length = 24
+# prediction_length = 3
+# model = get_model(lstm_layers, dense_layers, sequence_length, prediction_length, len(FEATURE_COLUMNS), dropout=0.2, recurrent_dropout=0.2)
+# predicted_label = 'load'
+# epochs = 10
+# file_name = get_model_name(lstm_layers, dense_layers, sequence_length, prediction_length, epochs)
+#
+# train_model(model, sequence_length, prediction_length, predicted_label, epochs, file_name=file_name)
+
+
+lstm_layers = [32]
+dense_layers = [16, 8]
+sequence_length = 24
+prediction_length = 3
+model = get_model(lstm_layers, dense_layers, sequence_length, prediction_length, len(FEATURE_COLUMNS), dropout=0.2, recurrent_dropout=0.2)
+predicted_label = 'load'
+epochs = 15
+file_name = get_model_name(lstm_layers, dense_layers, sequence_length, prediction_length, epochs)
+
+train_model(model, sequence_length, prediction_length, predicted_label, epochs, file_name=file_name)
