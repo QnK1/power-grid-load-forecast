7aa52219f5463597eb1b897379a7eaaf7d1d8065
MrWlobo <michaldworniczak40@gmail.com>
2025-11-03 10:05:44 +0100

    Added some sample prediction plots as an example

--- eval/lstm/evaluer.py
+++ eval/lstm/evaluer.py
@@ -6,4 +6,4 @@ from eval.result_plotter import plot_predictions
 file_name = 'LSTM_64-32_DENSE_16-16_168_24_50'
 y_real, y_pred = evaluate_models(168, 24, file_name)
 
-plot_predictions(y_real, y_pred, [10, 43, 235, 3425], file_name)
+plot_predictions(y_real, y_pred, [3, 10, 42, 43, 235, 888, 2094, 2137, 3000, 3425, 6780, 10000, 15151], file_name)
