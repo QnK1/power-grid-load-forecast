4a6da4140c6d57529bbcbbdafed7c86fc97b509e
MrWlobo <michaldworniczak40@gmail.com>
2025-11-09 19:01:50 +0100

    Added monthly line plots comparing load and temperature (normalized to 0-1) for each year in the training data set.

--- analysis/plot_creation.py
+++ analysis/plot_creation.py
@@ -2,11 +2,33 @@ import numpy as np
 
 from analysis.input_data_analysis import InputDataPlotCreator
 
-plot_creator = InputDataPlotCreator([2015], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+#plot_creator = InputDataPlotCreator([2015], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
 # plot_creator.create_line_chart_with_index(["load", "temperature"], normalize=True, save_plot=False, show_plot=True)
 # plot_creator.create_correlation_matrix(show_plot=True, title="July 2015 - 2019")
 # plot_creator.create_correlation_matrices(show_plot=True)
 # plot_creator.create_bar_chart(np.mean, "temperature", "day_of_week", normalize=False, save_plot=False, show_plot=True)
 # plot_creator.create_bar_chart(sum, "load", normalize=False, save_plot=False, show_plot=True)
 #plot_creator.create_qq_plot('load', 'temperature', 100, show_plot=True, save_plot=True)
-plot_creator.create_auto_correlation_plot('load', 960, show_plot=True, save_plot=True)
+#plot_creator.create_auto_correlation_plot('load', 960, show_plot=True, save_plot=True)
+years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+months = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+month_names = {
+            0: 'January',
+            1: 'February',
+            2: 'March',
+            3: 'April',
+            4: 'May',
+            5: 'June',
+            6: 'July',
+            7: 'August',
+            8: 'September',
+            9: 'October',
+            10: 'November',
+            11: 'December'
+        }
+
+for year in years:
+    for month in months:
+        plot_creator = InputDataPlotCreator([year], [month])
+        plot_creator.create_line_chart_with_index(["load", "temperature"], title=f"{month_names[month]} {year}", filename=f"{month_names[month]}{year}", normalize=True, save_plot=True, show_plot=False)
--- eval/lstm/evaluer.py
+++ eval/lstm/evaluer.py
@@ -3,8 +3,8 @@ from analysis.model_analysis import ModelPlotCreator
 
 # Temporary file to perform model evaluation
 
-file_name = 'LSTM_8_DENSE_8_84_12_11_2h'
-y_real, y_pred = evaluate_models(84, 12, file_name, freq="2h")
+file_name = 'LSTM_64-32_DENSE_32-16_168_24_51_1h'
+y_real, y_pred = evaluate_models(168, 24, file_name, freq="1h")
 
 plot_creator = ModelPlotCreator()
-plot_creator.plot_predictions(y_real[6], y_pred[6],  file_name, "lstm", freq='2h', save_plot=False, show_plot=True)
+plot_creator.plot_predictions(y_real[6], y_pred[6],  file_name, "lstm", freq='1h', save_plot=False, show_plot=True)
