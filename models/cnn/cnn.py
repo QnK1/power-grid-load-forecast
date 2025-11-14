6c658155ae2ddbe9ecdec471473f77676e394ad6
MrWlobo <michaldworniczak40@gmail.com>
2025-11-14 21:14:05 +0100

    Added function to train CNN models.

--- models/cnn/cnn.py
+++ models/cnn/cnn.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from keras.src.optimizers import Adam
 from models.helpers import create_sequences
 from utils.load_data import load_training_data, DataLoadingParams
@@ -108,3 +110,78 @@ def get_model(convolution_layers: list[int], kernel_sizes: list[int], dense_laye
     optimizer = Adam(learning_rate=0.001)
     model.compile(optimizer=optimizer, loss=MeanSquaredError(), metrics=['mse'])
     return model
+
+def train_model(model: keras.Sequential, sequence_length: int, prediction_length: int, predicted_label: str, epochs: int, batch_size: int = 32, freq: str = "1h", file_name: str = "cnn_model", early_stopping: bool = True) -> History:
+    """
+    Trains the provided 1D Convolutional Neural Network (CNN) model using historical time-series data.
+
+    Parameters
+    ----------
+    model : keras.Sequential
+        Compiled Keras CNN model created via `get_model`, ready for training.
+
+    sequence_length : int
+        Length of input sequences passed to the model (number of timesteps per sequence).
+
+    prediction_length : int
+        Number of timesteps into the future the model predicts.
+
+    predicted_label : str
+        Column name containing the target values to be predicted.
+
+    epochs : int
+        Number of training epochs.
+
+    batch_size : int, optional (default=32)
+        Number of samples per gradient update during training.
+
+    freq : str, optional (default="1h")
+        Data frequency used for training (e.g., "1h" for hourly data).
+
+    file_name : str, optional (default="cnn_model")
+        Name of the file under which the trained model will be saved.
+
+    early_stopping : bool, optional (default=True)
+        If True, uses EarlyStopping to monitor validation loss and stop training when
+        there is no improvement. Prevents overfitting and restores the best model weights.
+
+    Returns
+    -------
+    History
+        A Keras History object containing details about the training process,
+        including loss and metrics values for each epoch.
+    """
+
+    params = DataLoadingParams()
+    params.freq = freq
+    params.interpolate_empty_values = True
+    params.shuffle = False
+    params.include_timeindex = True
+
+    data, raw_data = load_training_data(params)
+    current_folder = Path(__file__).parent
+    model_folder = current_folder / 'models'
+    model_folder.mkdir(exist_ok=True)
+
+    X_train, y_train = create_sequences(data, sequence_length, prediction_length, FEATURE_COLUMNS, predicted_label)
+
+    early_stop = keras.callbacks.EarlyStopping(
+        monitor='val_loss',
+        min_delta=0.001,
+        patience=5,
+        restore_best_weights=True
+    )
+
+    history = model.fit(
+        X_train,
+        y_train,
+        epochs=epochs,
+        batch_size=batch_size,
+        validation_split = 0.1 if early_stopping else None,
+        callbacks = [early_stop] if early_stopping else [],
+        verbose=1)
+
+    model_path = model_folder / f'{file_name}.keras'
+    model.save(model_path)
+
+    return history
