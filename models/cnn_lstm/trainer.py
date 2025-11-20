fe34a5d4bcc0cfca0f1932eb636afdf9749e9f8d
MrWlobo <michaldworniczak40@gmail.com>
2025-11-20 20:26:20 +0100

    Added CNN-LSTM models and evals. Added CNN and CNN-LSTM MAPE over horizon plots.

--- models/cnn_lstm/trainer.py
+++ models/cnn_lstm/trainer.py
@@ -2,15 +2,15 @@ from models.cnn_lstm.cnn_lstm import get_model, get_model_name, train_model, FEA
 from analysis.model_analysis import ModelPlotCreator
 
 convolution_layers = [64]
-kernel_sizes = [3]
-lstm_layers = [16]
-dense_layers = [32, 16]
-sequence_length = 168
+kernel_sizes = [5]
+lstm_layers = [32]
+dense_layers = [64, 32]
+sequence_length = 4 * 168
 prediction_length = 24
 
 model = get_model(convolution_layers, kernel_sizes, lstm_layers, dense_layers, sequence_length, prediction_length, len(FEATURE_COLUMNS))
 predicted_label = 'load'
-epochs = 50
+epochs = 70
 file_name = get_model_name(convolution_layers, lstm_layers, dense_layers, sequence_length, prediction_length, epochs, 32, "1h")
 
 history = train_model(model, sequence_length, prediction_length, predicted_label, epochs, file_name=file_name, freq="1h")
